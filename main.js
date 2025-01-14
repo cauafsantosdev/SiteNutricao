5983b692a798d155eb056b42644bacfe49f33631
jpbdalmagro <jpbdalmagro@gmail.com>
2025-01-14 01:59:41 -0300

    novas atualizações

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const alimentos =[
     {
         "Alimento": "Arroz branco",
         "Carboidratos (mg)": 0.282,
-        "Proteínas (mg)": 0.027000000000000003,
+        "Proteínas (mg)": 0.027,
         "Gorduras (mg)": 3.0,
         "Fibras (mg)": 4.0,
         "Açúcares (mg)": 1.0,
@@ -10,7 +10,6 @@ const alimentos =[
         "Potássio (mg)": 0.35,
         "Cálcio (mg)": 0.1,
         "Vitaminas Principais": "-",
-        "Calorias": 1.3,
         "Imagem": "Arroz.jpg",
         "ID": "arroz"
     },
@@ -19,13 +18,12 @@ const alimentos =[
         "Carboidratos (mg)": 0.2,
         "Proteínas (mg)": 0.09,
         "Gorduras (mg)": 5.0,
-        "Fibras (mg)": 0.013000000000000001,
+        "Fibras (mg)": 0.013,
         "Açúcares (mg)": 0.01,
         "Sódio (mg)": 0.02,
         "Potássio (mg)": 0.015,
         "Cálcio (mg)": 0.5,
         "Vitaminas Principais": "B9",
-        "Calorias": 1.32,
         "Imagem": "Feijão.jpg",
         "ID": "feijao"
     },
@@ -40,22 +38,20 @@ const alimentos =[
         "Potássio (mg)": 1.2,
         "Cálcio (mg)": 0.2,
         "Vitaminas Principais": "-",
-        "Calorias": 2.7,
         "Imagem": "Pão.jpg",
         "ID": "pao"
     },
     {
         "Alimento": "Laranja",
-        "Carboidratos (mg)": 0.1175,
+        "Carboidratos (mg)": 0.117,
         "Proteínas (mg)": 9.4,
         "Gorduras (mg)": 1.2,
-        "Fibras (mg)": 0.018000000000000002,
-        "Açúcares (mg)": 0.0935,
+        "Fibras (mg)": 0.0182,
+        "Açúcares (mg)": 0.093,
         "Sódio (mg)": 0.01,
         "Potássio (mg)": 1.8,
         "Cálcio (mg)": 0.4,
         "Vitaminas Principais": "C, A",
-        "Calorias": 0.47,
         "Imagem": "Laranja.jpg",
         "ID": "laranja"
     },
@@ -70,7 +66,6 @@ const alimentos =[
         "Potássio (mg)": 3.5,
         "Cálcio (mg)": 0.15,
         "Vitaminas Principais": "-",
-        "Calorias": 2.5,
         "Imagem": "Carne.jpg",
         "ID": "carne"
     },
@@ -78,14 +73,13 @@ const alimentos =[
         "Alimento": "Carne de frango",
         "Carboidratos (mg)": 0.0,
         "Proteínas (mg)": 0.31,
-        "Gorduras (mg)": 0.036000000000000004,
+        "Gorduras (mg)": 0.036,
         "Fibras (mg)": 0.0,
         "Açúcares (mg)": 0.0,
         "Sódio (mg)": 0.7,
         "Potássio (mg)": 3.0,
         "Cálcio (mg)": 0.12,
         "Vitaminas Principais": "-",
-        "Calorias": 2.39,
         "Imagem": "Frango.jpg",
         "ID": "frango"
     },
@@ -100,7 +94,6 @@ const alimentos =[
         "Potássio (mg)": 1.26,
         "Cálcio (mg)": 0.56,
         "Vitaminas Principais": "B12, D",
-        "Calorias": 1.55,
         "Imagem": "Ovo.jpg",
         "ID": "ovo"
     },
@@ -115,8 +108,7 @@ const alimentos =[
         "Potássio (mg)": 0.9,
         "Cálcio (mg)": 0.0,
         "Vitaminas Principais": "-",
-        "Calorias": 0.0,
-        "Imagem": "Cafe.jpg",
+        "Imagem": "Café.jpg",
         "ID": "cafe"
     },
     {
@@ -124,28 +116,26 @@ const alimentos =[
         "Carboidratos (mg)": 0.2284,
         "Proteínas (mg)": 0.0109,
         "Gorduras (mg)": 3.3,
-        "Fibras (mg)": 0.026000000000000002,
-        "Açúcares (mg)": 0.1223,
+        "Fibras (mg)": 0.026,
+        "Açúcares (mg)": 0.122,
         "Sódio (mg)": 0.01,
         "Potássio (mg)": 3.6,
         "Cálcio (mg)": 0.05,
         "Vitaminas Principais": "B6, C",
-        "Calorias": 0.89,
         "Imagem": "Banana.jpg",
         "ID": "banana"
     },
     {
         "Alimento": "Batata branca",
-        "Carboidratos (mg)": 0.17579999999999998,
-        "Proteínas (mg)": 0.0202,
+        "Carboidratos (mg)": 0.175,
+        "Proteínas (mg)": 0.022,
         "Gorduras (mg)": 1.0,
-        "Fibras (mg)": 0.022000000000000002,
+        "Fibras (mg)": 0.022,
         "Açúcares (mg)": 0.0125,
         "Sódio (mg)": 0.06,
         "Potássio (mg)": 4.25,
         "Cálcio (mg)": 0.1,
         "Vitaminas Principais": "C",
-        "Calorias": 0.79,
         "Imagem": "Batata.jpg",
         "ID": "batata"
     }
@@ -166,3 +156,27 @@ function mudarAba(event, tabId) {
     // Mostrar o conteúdo associado à aba clicada
     document.getElementById(tabId).classList.add('active');
 }
+
+$(document).ready(function() {
+    alimentos.forEach(alimento => {
+        const card = `
+            <div class="card">
+                <img src="imagens/${alimento.Imagem}" alt="${alimento.Alimento}">
+                <h3>${alimento.Alimento}</h3>
+                <p>Carboidratos: ${alimento["Carboidratos (mg)"]}mg</p>
+                <p>Proteínas: ${alimento["Proteínas (mg)"]}mg</p>
+                <p>Gorduras: ${alimento["Gorduras (mg)"]}mg</p>
+                <p>Fibras: ${alimento["Fibras (mg)"]}mg</p>
+                <p>Açúcares: ${alimento["Açúcares (mg)"]}mg</p>
+                <p>Sódio: ${alimento["Sódio (mg)"]}mg</p>
+                <p>Potássio: ${alimento["Potássio (mg)"]}mg</p>
+                <p>Cálcio: ${alimento["Cálcio (mg)"]}mg</p>
+                <p>Vitaminas Principais: ${alimento["Vitaminas Principais"]}</p>
+            </div>
+        `;
+
+        $('.cards').append(card);
+    }
+)}
+)
+
